658e7627b59c735054f3eccc80b292bf359a1b45
Ritika <ritikadas98@gmail.com>
2025-01-29 13:42:26 +0530

    initial commit: add QuoteGenerator application with HTML, CSS, and JavaScript files

---
+++ app.js
@@ -0,0 +1,29 @@
+const api_url= "https://api.quotable.io/random";
+
+const quote = document.getElementById ("quote")
+const author = document.getElementById ("author")
+
+async function getQuote(url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    console.log (data);
+    quote.innerHTML= data.content;
+    author.innerHTML= data.author;
+    event.preventDefault()
+}
+
+getQuote(api_url);
+
+
+// function callurl() {
+//     getquote(api_url)
+// }
+
+// const newQuote = document.getElementsByClassName (".newQuote");
+ 
+// newQuote.addEventListener('click'.callurl)
+
+function tweet()
+{
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "----by" + " " + author.innerHTML, "Tweet Window", "width=800 height=400");
+}
