e8b03055ef1c4837a1e190188ce27839a7a3b3dd
Ritika <ritikadas98@gmail.com>
2025-01-30 13:57:24 +0530

    feat: add functionality to fetch and display new quotes, and implement tweet feature

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 const api_url= "https://api.quotable.io/random";
 
-const quote = document.getElementById ("quote")
-const author = document.getElementById ("author")
+const quote = document.getElementById("quote")
+const author = document.getElementById("author")
+const newQuoteBtn = document.getElementById("new_quote")
+const tweetBtn = document.getElementById("new_tweet")
+
+// console.log(newQuoteBtn);
 
 async function getQuote(url) {
-    const response = await fetch(url);
-    let data = await response.json();
-    console.log (data);
-    quote.innerHTML= data.content;
-    author.innerHTML= data.author;
-    event.preventDefault()
+    try{
+        // console.log("fetching data");
+        const response = await fetch(url);
+        let data = await response.json();
+        console.log (data);
+        quote.innerHTML= data.content;
+        author.innerHTML= data.author;
+
+    } catch (error){
+        console.error("error", error);
+    }
+   
 }
 
 getQuote(api_url);
 
+function getNewQuote(){
+    // console.log("new quote button clicked");
+    getQuote(api_url);
+}
+
+newQuoteBtn.addEventListener("click", getNewQuote);
 
 function tweet()
 {
     window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "----by" + " " + author.innerHTML, "Tweet Window", "width=800 height=400");
 }
+
+tweetBtn.addEventListener("click", tweet);
